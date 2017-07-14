ddb14ddcd06e67e01830dd085d364d736e06d757
Lilit Avetisyan <lilitavetisyan97@gmail.com>
2017-07-14 12:19:43 +0400

    agenda

---
+++ iefa/IEFAAgendaViewController.h
@@ -0,0 +1,13 @@
+//
+//  IEFAAgendaViewController.h
+//  iefa
+//
+//  Created by Lilit Avetisyan on 7/13/17.
+//  Copyright © 2017 Paruyr Danielian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IEFAAgendaViewController : UIViewController
+
+@end
